b68f4ae82a1bf5fb5722c9c69a53c299b4bfd988
drew-kd-rodgers <andrew.k.d.rodgers@gmail.com>
2025-05-02 11:15:20 +0100

    Add jest testing file

---
+++ decoder.test.js
@@ -0,0 +1,16 @@
+const {shift_text, caesar_cipher} = require("./decoder")
+
+test("test alphabet shift", () => {
+    expect(shift_text("a", 3)).toBe("d");
+    expect(shift_text("a", -1)).toBe("z");
+    expect(shift_text(" ", 4)).toBe(" ");
+    expect(shift_text("a", 26)).toBe(shift_text("a", 0));
+    expect(shift_text("Z", 1)).toBe("A");
+    expect(shift_text("AAAAAA", 1)).toBe("BBBBBB");
+})
+
+test("test caesar cipher", () => {
+    expect(caesar_cipher("DDD", 2, false)).toBe("BBB");
+    expect(caesar_cipher("DDD", 2, true)).toBe("FFF");
+    expect(caesar_cipher("Secret message", 3, false)).toBe("Pbzobq jbppxdb");
+})
